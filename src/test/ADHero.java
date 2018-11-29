50f40ed2bb94d97741a0154150de67e351dad8ff
李晓晗 <514504871@qq.com>
2018-11-29 17:34:16 +0800

    add some test

---
+++ src/test/ADHero.java
@@ -0,0 +1,7 @@
+package test;
+
+public class ADHero extends Hero{
+	public void ADAttack() {
+		System.out.println(name+"发动了物理攻击");
+	}
+}
---
+++ src/test/APHero.java
@@ -0,0 +1,7 @@
+package test;
+
+public class APHero extends Hero{
+	public void APAttack() {
+		System.out.println(name+":发动了魔法攻击");
+	}
+}
